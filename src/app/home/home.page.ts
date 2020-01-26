26db0fd160cfeaa846122a67a73dab926b1ed194
gustavo9601 <tavo9601@gmail.com>
2020-01-26 15:03:44 -0500

    Init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ToastController} from '@ionic/angular';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor(public toastController: ToastController) {
+    }
+
+
+    op:boolean= false;
+
+
+    async mostrarToast() {
+        const toast = await this.toastController.create({
+            message: (this.op) ? 'UMB Agregada a favoritos': 'Bienvenido a la UMB Virtual.',
+            duration: 2000
+        });
+        toast.present();
+    }
+
+
+    async mostrarToastConOpciones() {
+        const toast = await this.toastController.create({
+            header: 'Mensaje',
+            message: 'Click para cerrar',
+            position: 'top',
+            buttons: [
+                {
+                    side: 'start',
+                    icon: 'star',
+                    text: 'Favorito',
+                    handler: () => {
+                        this.op = true;
+                        this.mostrarToast();
+                        this.op = false;
+                    }
+                }, {
+                    text: 'Ok',
+                    role: 'cancel',
+                    handler: () => {
+                        return;
+                    }
+                }
+            ]
+        });
+        toast.present();
+    }
+
 
 }
